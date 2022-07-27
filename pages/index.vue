b7f359a2ac82bb1ed03d7d12f560c87236e4e378
Kyle Jennings <kylejennings@Kyles-MacBook-Air.local>
2022-07-27 09:42:17 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  async beforeMount () {
+  try {
+    const results = await this.api('me');
+    console.log(results);
+  } catch (e) {
+    console.log(e);
+  }
+}
+})
+</script>
